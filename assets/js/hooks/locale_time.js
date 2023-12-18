7f3466073b02a47504f52571df582b6067acaf02
Moosieus <cameron.duley99@gmail.com>
2023-12-18 15:15:52 -0500

    fix locale_time hook

--- assets/js/hooks/locale_time.js
+++ assets/js/hooks/locale_time.js
@@ -1,6 +1,16 @@
 export default {
     // need one or more of: mounted | beforeUpdated | updated
-    mounted() {        
+    mounted() {
+        const t = this.el.textContent.split(":")
+                
+        const d = new Date()
+        d.setUTCHours(t[0])
+        d.setUTCMinutes(t[1])
+        d.setUTCSeconds(t[2])
+
+        this.el.textContent = d.toLocaleTimeString()
+    },
+    updated() {
         const t = this.el.textContent.split(":")
                 
         const d = new Date()
