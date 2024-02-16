eef74289115d938ea983876d1565f0ec6975662e
Moosieus <cameron.duley99@gmail.com>
2024-02-15 20:28:41 -0500

    update `:last_changed` to be `DateTime`

--- assets/js/hooks/locale_time.js
+++ assets/js/hooks/locale_time.js
@@ -1,23 +1,15 @@
+export function localizeTimestamp() {
+    const t = this.el.textContent.split(":")
+    
+    const d = new Date()
+    d.setUTCHours(t[0])
+    d.setUTCMinutes(t[1])
+    d.setUTCSeconds(t[2])
+
+    this.el.textContent = d.toLocaleTimeString()
+}
+
 export default {
-    // need one or more of: mounted | beforeUpdated | updated
-    mounted() {
-        const t = this.el.textContent.split(":")
-                
-        const d = new Date()
-        d.setUTCHours(t[0])
-        d.setUTCMinutes(t[1])
-        d.setUTCSeconds(t[2])
-
-        this.el.textContent = d.toLocaleTimeString()
-    },
-    updated() {
-        const t = this.el.textContent.split(":")
-                
-        const d = new Date()
-        d.setUTCHours(t[0])
-        d.setUTCMinutes(t[1])
-        d.setUTCSeconds(t[2])
-
-        this.el.textContent = d.toLocaleTimeString()
-    }
+    mounted: localizeTimestamp,
+    updated: localizeTimestamp,
 }
